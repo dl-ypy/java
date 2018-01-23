69405c168a948488f0529b2b0b1c395dbe0738c6
ypy <792015431@qq.com>
2018-01-23 21:14:41 +0800

    排序算法

--- 算法/排序/Sort.java
+++ 算法/排序/Sort.java
@@ -3,6 +3,8 @@ package javaTest;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * @author joker
@@ -12,12 +14,16 @@ public class Test{
 	static Integer[] arr = new Integer[]{5,66,9,0,-1,42,77}; 
 	
 	public static void main(String[] args) {
+		System.out.println("排序前："+Arrays.toString(arr));
 		selectSort(arr);
 		System.out.println("选择排序："+Arrays.toString(arr));
 		//转换为list将顺序重新打乱
 		List<Integer> list = Arrays.asList(arr);
 		Collections.shuffle(list);
 		arr = (Integer[])list.toArray(new Integer[list.size()]);
+		System.out.println("排序前："+Arrays.toString(arr));
+		directInsertSort(arr);
+		System.out.println("直接排序："+Arrays.toString(arr));
 	}
 	
 	/*
@@ -41,9 +47,33 @@ public class Test{
 			}
 		}
 	}
+	
+	/*
+	 * 直接插入排序
+	 * 描述：与前面的比较，循环将比当前数小的插入当前数后面，直到遇到小于等于当前数的，break，最后将初始时比较的数放到当前j的下一位
+	 * 时间复杂度：n+(n-1)+...+1=n(n+1)/2
+	 * */
+	public static void directInsertSort(Integer[] arr) {
+		Integer min;
+		for (Integer i=0; i<arr.length; i++) {
+			min = arr[i];
+			int j;
+			for (j=i-1; j>=0; j--) {
+				if (arr[j] > min) {
+					arr[j+1] = arr[j];
+				} else {
+					break;
+				}
+			}
+			arr[j+1] = min;
+		}
+	}
 }
 
 /**
  * JDK1.8运行结果
+ * 排序前：[5, 66, 9, 0, -1, 42, 77]
  * 选择排序：[-1, 0, 5, 9, 42, 66, 77]
+ * 排序前：[5, 77, 42, 9, -1, 0, 66]
+ * 直接排序：[-1, 0, 5, 9, 42, 66, 77]
 */
